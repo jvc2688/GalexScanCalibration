c421330e70d55c0fefc79e7836149d2d012e8461
jvc2688 <wsdun1029@gmail.com>
2015-11-29 13:31:26 -0500

    change name

--- code/spilt_csv.py
+++ code/spilt_csv.py
@@ -1,23 +1,32 @@
 import csv
 import cPickle as pickle
 import sys
+import os
 
 def save_obj(obj, name ):
-    with open('../data/'+ name + '.pkl', 'wb') as f:
+    with open(name + '.pkl', 'wb') as f:
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 def load_obj(name ):
-    with open('../data/' + name + '.pkl', 'rb') as f:
+    with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
 
 if __name__ == '__main__':
-	scan_num = sys.argv[1]
+	csvname = sys.argv[1]
+
+	output = "../data/%s/split/%s.csv"%(csvname, csvname)
+	dir = os.path.dirname(output)
+	if not os.path.exists(dir):
+		os.makedirs(dir)
+	else:
+		print 'exists'
+		exit()
 	look_up = {}
 	i=0
-	with open('../data/photon_list/NUVPhoton%s_full_new.csv'%scan_num, 'rb') as f:
+	with open('../data/photon_list/%s.csv'%csvname, 'rb') as f:
 		reader = csv.reader(f)
 		filename = '%d.csv'%i
-		w = open('../data/split%s_new/'%scan_num+filename, 'wb')
+		w = open('../data/%s/split/'%csvname+filename, 'wb')
 		writer = csv.writer(w)
 		row = reader.next()
 		writer.writerow(row)
@@ -30,12 +39,12 @@ if __name__ == '__main__':
 				filename = '%d.csv'%i
 				last = time
 				w.close()
-				w = open('../data/split%s_new/'%scan_num+filename, 'wb')
+				w = open('../data/%s/split/'%csvname+filename, 'wb')
 				writer = csv.writer(w)
 				print time, filename
 			writer.writerow(row)
 			look_up[row[0]] = i
-	save_obj(look_up, 'lookup%s_new'%scan_num)
+	save_obj(look_up, '../data/%s/split/lookup'%csvname)
 
 
 
